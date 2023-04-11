88f6de862d0f76b4625c3d71726b97f2a30a023e
Akhyar <ahyar12324@gmail.com>
2023-04-11 22:44:18 +0700

    feat:modify index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,52 +2,53 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>NutriCycle</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-  <script src="https://kit.fontawesome.com/348c676099.js" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>NutriCycle</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/348c676099.js" crossorigin="anonymous"></script>
 </head>
 
 <main>
 
-  <main>
+    <main>
 
-    <div class="navbar navbar-expand-lg" style="background-color: #13192B;">
-      <div class="container-fluid">
-        <div class="navbar-brand text-white pe-3" style="border-right: 1px solid white;">
-          <img src="images/logo.png" alt="Logo NutriCycle" width="40"> NutriCycle
+        <!-- ini adalah bagian navbar-->
+        <div class="navbar navbar-expand-lg" style="background-color: #13192B;">
+            <div class="container-fluid">
+                <div class="navbar-brand text-white pe-3" style="border-right: 1px solid white;">
+                    <img src="images/logo.png" alt="Logo NutriCycle" width="40"> NutriCycle
+                </div>
+                <div class="text-white collapse navbar-collapse">Landing Page</div>
+            </div>
         </div>
-        <div class="text-white collapse navbar-collapse">Landing Page</div>
-      </div>
-    </div>
 
-    <br><br><br>
+        <br><br><br>
 
-    <center class="text-white">
-      <h1 style="font-size: 5rem;">Welcome</h1>
-      <br>
-      <a href="sign.html" class="text-decoration-none">
-        <img src="images/logo.png" alt="">
-      </a>
-      <br><br>
+        <center class="text-white">
+            <h1 style="font-size: 5rem;">Welcome</h1>
+            <br>
+            <a href="sign.html" class="text-decoration-none">
+                <img src="images/logo.png" alt="">
+            </a>
+            <br><br>
 
-      <h2>Ubah Sampah Beranak <br>
-        Menjadi Pakan Ternak</h2>
-      <br><br><br>
-    </center>
+            <h2>Ubah Sampah Beranak <br>
+                Menjadi Pakan Ternak</h2>
+            <br><br><br>
+        </center>
 
-  </main>
+    </main>
 
-  <nav style="background-color:white;" class="text-center pt-3">
-    <p>Copyright 2023 &copy; NutriCycle</p>
-  </nav>
+    <nav style="background-color:white;" class="text-center pt-3">
+        <p>Copyright 2023 &copy; NutriCycle</p>
+    </nav>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </main>
 
 </body>
